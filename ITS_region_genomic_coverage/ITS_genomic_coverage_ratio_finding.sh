f15bf8d9f2b6198f4bdf376f9c8e2eb8c0034064
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-06-09 09:56:38 +0100

    lots of echo commands added

--- ITS_region_genomic_coverage/ITS_genomic_coverage_ratio_finding.sh
+++ ITS_region_genomic_coverage/ITS_genomic_coverage_ratio_finding.sh
@@ -50,6 +50,7 @@ cd ${genome_prefix}
 
 # i get my reads from: http://www.ebi.ac.uk/ena/data/warehouse/search
 # use the advance.  Species ENTER, Lib.layout=paired,  platform=illumina, lib.source=genomic > search
+echo STEP1: Downloading reads and genome files
 wget ${read_1_link}
 wget ${read_2_link}
 wget ${genome_fasta}
@@ -61,16 +62,22 @@ gunzip ${genome_prefix}*
 
 # blast to get representative ITS regions.
 makeblastdb -in ${genome_prefix}*.fa -dbtype nucl
-blastn -query ${path_to_ITS_clipping_file}/P.infestnas_ITS.fasta -db ${genome_prefix}*.fa -outfmt 6 -out n.Pi_ITS_vs_${genome_prefix}.out
+echo makeblastdb -in ${genome_prefix}*.fa -dbtype nucl
 
+blastn -query ${path_to_ITS_clipping_file}/P.infestnas_ITS.fasta -db ${genome_prefix}*.fa -outfmt 6 -out n.Pi_ITS_vs_${genome_prefix}.out
+echo Blast: blastn -query ${path_to_ITS_clipping_file}/P.infestnas_ITS.fasta -db ${genome_prefix}*.fa -outfmt 6 -out n.Pi_ITS_vs_${genome_prefix}.out
 # prepare ITS gff. python:
 
 python ${path_to_ITS_clipping_file}/generate_ITS_GFF.py --blast n.Pi_ITS_vs_${genome_prefix}.out --prefix ${genome_prefix} -o ${genome_prefix}.ITS.GFF
+echo python ${path_to_ITS_clipping_file}/generate_ITS_GFF.py --blast n.Pi_ITS_vs_${genome_prefix}.out --prefix ${genome_prefix} -o ${genome_prefix}.ITS.GFF
 
 wait
 #quality trim the reads
 # head crop 9 as all illumina data seems to have non-random bases at frist 9-12 nt. Weird!
 java -jar ${trimmomatic_path}/trimmomatic-0.32.jar PE -threads ${num_threads} -phred33 ${SRA_prefix}_1.fastq.gz ${SRA_prefix}_2.fastq.gz R1.fq.gz unpaired_R1.fq.gz R2.fq.gz unpaired_R2.fq.gz ILLUMINACLIP:${path_to_ITS_clipping_file}/TruSeq3-PE.fa:2:30:10 LEADING:3 HEADCROP:9 TRAILING:3 SLIDINGWINDOW:4:22 MINLEN:51
+echo Trimming reads: java -jar ${trimmomatic_path}/trimmomatic-0.32.jar PE -threads ${num_threads} -phred33 ${SRA_prefix}_1.fastq.gz ${SRA_prefix}_2.fastq.gz R1.fq.gz unpaired_R1.fq.gz R2.fq.gz unpaired_R2.fq.gz ILLUMINACLIP:${path_to_ITS_clipping_file}/TruSeq3-PE.fa:2:30:10 LEADING:3 HEADCROP:9 TRAILING:3 SLIDINGWINDOW:4:22 MINLEN:51
+
+
 wait
 rm  unpaired_R*
 
@@ -79,6 +86,7 @@ mkdir $TMP
 
 # index genome
 bowtie2-build -f ${genome_prefix}*.fa bowtie_index_files
+echo bowtie2-build -f ${genome_prefix}*.fa bowtie_index_files
 
 # randomly assign mutliple mapping reads ...  http://bowtie-bio.sourceforge.net/bowtie2/manual.shtml#bowtie2-options-p
 #OLD command: bowtie2 --very-sensitive --non-deterministic --seed 1 --no-mixed --no-unal -p 8 -x Pi -1 R1.fq.gz -2 R2.fq.gz -S P.nicotiana.sam
@@ -86,12 +94,18 @@ bowtie2-build -f ${genome_prefix}*.fa bowtie_index_files
 #pipe SAM output on stdout into samtools to make it into BAM
 #TODO: Put the temp unsorted BAM file on the local hard drive scratch space under /mnt/scratch
 bowtie2 --very-sensitive --non-deterministic --seed 1 --no-mixed --no-unal -p ${num_threads} -x bowtie_index_files -1 R1.fq.gz -2 R2.fq.gz | samtools view -S -b -o $TMP/tmp_unsorted.bam -
+echo MAPPING READS: bowtie2 --very-sensitive --non-deterministic --seed 1 --no-mixed --no-unal -p ${num_threads} -x bowtie_index_files -1 R1.fq.gz -2 R2.fq.gz | samtools view -S -b -o $TMP/tmp_unsorted.bam -
+
 
 # convert to sorted bam.
 #samtools view -@ 8 -S -b -o tmp_unsorted.bam P.nicotiana.sam
 
 samtools sort -@ ${num_threads} $TMP/tmp_unsorted.bam ${genome_prefix}
+echo samtools sort -@ ${num_threads} $TMP/tmp_unsorted.bam ${genome_prefix}
+
 samtools index ${genome_prefix}.bam
+echo samtools index ${genome_prefix}.bam
+
 
 # Clean up, doing this last in case fails
 rm $TMP/tmp_unsorted.bam
@@ -102,23 +116,29 @@ rmdir $TMP
 
 # get only the genes, not bothered about other stuff ...
 cat ${genome_prefix}*gff3 | grep "ID=gene" | grep -v "mRNA" > ${genome_prefix}.gene.gff
+echo cat ${genome_prefix}*gff3 | grep "ID=gene" | grep -v "mRNA" > ${genome_prefix}.gene.gff
 
 # use bedtools to get the number of reads that map to specific regions
 
 bedtools multicov -bams ${genome_prefix}.bam -bed ${genome_prefix}.gene.gff > ${genome_prefix}_genomic.genes.cov
+echo bedtools multicov -bams ${genome_prefix}.bam -bed ${genome_prefix}.gene.gff > ${genome_prefix}_genomic.genes.cov
 
 bedtools multicov -bams ${genome_prefix}.bam -bed ${genome_prefix}.ITS.GFF > ${genome_prefix}_genomic.ITS.cov
+echo bedtools multicov -bams ${genome_prefix}.bam -bed ${genome_prefix}.ITS.GFF > ${genome_prefix}_genomic.ITS.cov
 
 
 # just get the values using cut
 # remove RNA genes from the all gene values, as these are what we are measuring in the other
 # dataset. Assuming they are annotated in the GFF. It not... results may be a bit skewed.
 cat ${genome_prefix}_genomic.genes.cov | grep -v "RNA" | cut -f10 > ${genome_prefix}_genomic.genes.cov.values
+echo cat ${genome_prefix}_genomic.genes.cov | grep -v "RNA" | cut -f10 > ${genome_prefix}_genomic.genes.cov.values
 
 cat  ${genome_prefix}_genomic.ITS.cov | cut -f10 >  ${genome_prefix}_genomic.ITS.cov.values
+echocat  ${genome_prefix}_genomic.ITS.cov | cut -f10 >  ${genome_prefix}_genomic.ITS.cov.values
 
 # get stats summary of coverages
 python ${path_to_ITS_clipping_file}/summary_stats.py --ITS ${genome_prefix}_genomic.ITS.cov.values --GFF ${genome_prefix}.ITS.GFF --all_genes_cov ${genome_prefix}_genomic.genes.cov.values -o ${genome_prefix}_stats.out
+echo python ${path_to_ITS_clipping_file}/summary_stats.py --ITS ${genome_prefix}_genomic.ITS.cov.values --GFF ${genome_prefix}.ITS.GFF --all_genes_cov ${genome_prefix}_genomic.genes.cov.values -o ${genome_prefix}_stats.out
 
 
 
