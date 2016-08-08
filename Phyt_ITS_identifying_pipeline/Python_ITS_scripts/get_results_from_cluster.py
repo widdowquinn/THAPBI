4b414d1f8687e0dfdd87d0a4110a290a138a1c8e
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-08-08 10:05:28 +0100

    working, added option to get reads of interest

--- Phyt_ITS_identifying_pipeline/Python_ITS_scripts/get_results_from_cluster.py
+++ Phyt_ITS_identifying_pipeline/Python_ITS_scripts/get_results_from_cluster.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #title: Parse clusters and find the phy spcies
 #in the cluster
 #author: Peter Thorpe September 2016. The James Hutton Insitute, Dundee, UK.
@@ -10,10 +9,7 @@ from optparse import OptionParser
 import datetime
 import os
 from sys import stdin,argv
-from Bio.SeqIO.QualityIO import FastqGeneralIterator
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
+
 import gzip
 
 
@@ -56,8 +52,29 @@ def get_barcode_seq (read, in_fastq):
     """
     return True
 
+def parse_line(line):
+    """finction to parse line"""
+    if not line.strip():
+        return False #if the last line is blank
+    if line.startswith("#"):
+        return False
+    #if not "Phytophthora" or "P." in line:
+        #continue
+    line = line.rstrip()
+    out_put_str = ""
+    cluster_line = line.rstrip("\n").split("\t")
+    # are the database Phy singletons? - then we wont be interested
+    number_of_reads_hitting_species = len(cluster_line)
+    #set up some blank variables
+    species = ""
+    barcode_left = ""
+    barcode_right = ""
+    #database phy counter
+    phy_count = 0
+    return cluster_line, number_of_reads_hitting_species, species, barcode_left, barcode_right, phy_count
 
-def parse_tab_file_get_clusters(filename1, left, right, barcode_length, out_file):
+
+def parse_tab_file_get_clusters(filename1, left, right, barcode_length, show_me_the_reads, out_file):
     """#script to open up a tab separeted clustering output and identify the
     species in the clustering"""
     #print coded_name_to_species_dict
@@ -67,30 +84,23 @@ def parse_tab_file_get_clusters(filename1, left, right, barcode_length, out_file
     cluster_file = open (filename1, "r")
     summary_out_file = open(out_file, "w")
     #title for the results file
-    title = "#species\tnumber_of_reads_hitting_species\tbarcodeR1\tbarcodeR2\n"
+    if show_me_the_reads:
+        title = "#species\tnumber_of_reads_hitting_species\tbarcodeR1\tbarcodeR2\treads_that_hit_species\n"
+    else:
+        title = "#species\tnumber_of_reads_hitting_species\tbarcodeR1\tbarcodeR2\n"
     summary_out_file.write(title)
     count = int(0)
     for line in cluster_file:
         count +=1
-        if not line.strip():
-            continue #if the last line is blank
-        if line.startswith("#"):
+        #call function to parse line
+        if not parse_line(line):
             continue
-        if not "Phytophthora" or "P." in line:
-            continue
-        line = line.rstrip()
-        out_put_str = ""
-        cluster_line = line.rstrip("\n").split("\t")
-        # are the database Phy singletons? - then we wont be interested
-        number_of_reads_hitting_species = len(cluster_line)
+        reads_of_interest = ""
+        cluster_line, number_of_reads_hitting_species, \
+                      species, barcode_left, barcode_right, phy_count = parse_line(line)
 
-        #set up some blank variables
-        species = ""
-        barcode_left = ""
-        barcode_right = ""
-        #database phy counter
-        phy_count = 0
         for member in cluster_line:
+            member = member.rstrip()
             #is a memeber of the database in this cluster?
             if member.startswith("Phytophthora"):
                 # yes we are interested in this cluster
@@ -99,23 +109,35 @@ def parse_tab_file_get_clusters(filename1, left, right, barcode_length, out_file
                 species = species+member+" "
                 #remove all the memebr which are database memebers
                 number_of_reads_hitting_species = number_of_reads_hitting_species-1
-        if number_of_reads_hitting_species >1 and phy_count >0: #after the line, are there more memeber that are not database members?
+        if number_of_reads_hitting_species >=1 and phy_count >0: #after the line, are there more memeber that are not database members?
             for member in cluster_line:
                 if "Phytophthora" in member:
                     #no barcode for these - obviously!
                     continue
                 print ("YYYEEEESSSSSS")
+                print cluster_line
+                # we do not add the reads that hit the 
+                reads_of_interest = reads_of_interest+member+" "
                 #call func to get the bar codes
+                print "I am getting the left bar code", left_read_to_barcode_dic[member]
                 left_barcode = left_read_to_barcode_dic[member]
                 # add to str
                 barcode_left = barcode_left+left_barcode+" "
+                print "I am getting the right bar code", right_read_to_barcode_dic[member]
                 right_barcode = right_read_to_barcode_dic[member]
                 #add to str
                 barcode_right = barcode_right+right_barcode+" "
-        data_output = "%s\t%d\t%s\t%s\t\n" %(species.rstrip(), number_of_reads_hitting_species,\
+            #format the data
+            if show_me_the_reads:
+                data_output = "%s\t%d\t%s\t%s\t%s\n" %(species.rstrip(), number_of_reads_hitting_species,\
+                                                       barcode_left,\
+                                                       barcode_right, reads_of_interest)
+            else:
+                data_output = "%s\t%d\t%s\t%s\t\n" %(species.rstrip(), number_of_reads_hitting_species,\
                                                        barcode_left,\
                                                        barcode_right)
-        summary_out_file.write(data_output)
+            #write out the data
+            summary_out_file.write(data_output)
 
 
 
@@ -129,15 +151,13 @@ def parse_tab_file_get_clusters(filename1, left, right, barcode_length, out_file
 
 usage = """usage :
 
-this scripts return a 'master file' summarising what is in the clusters.
+this scripts to summarise what clusters with what Phy species.
 
 
 python parse_clusters.py -i clustering_outfile_already_decoded_from_temp_names -o summarise_clusters.out
 
 command line option
 
-requires:
-biopython
 
 """
 
@@ -157,21 +177,32 @@ parser.add_option("-b", "--barcode_length", dest="barcode_length", default=8,
                   help="length of the barcode used. Default 8 ",
                   metavar="FILE")
 
+parser.add_option("-s", "--show_me_the_reads", dest="show_me_the_reads", default=False,
+                  help="show_me_the_reads in the output file for those that hit the Phy species."
+                  " by default this is off, as the file could get very large. -s True if you want ...",
+                  metavar="FILE")
+
 parser.add_option("-o", "--out_prefix", dest="out_file", default="summarise_clusters.out",
                   help="prefix to the output filenames")
 
 
 (options, args) = parser.parse_args()
-
+#-i
 in_file = options.in_file
+# -l
 left = options.left
+# -r
 right = options.right
+# -b
 barcode_length = options.barcode_length
+# -o
 out_file = options.out_file
+# -s
+show_me_the_reads = options.show_me_the_reads
 
 #run the program
 barcode_length = int(barcode_length)
 
-parse_tab_file_get_clusters(in_file, left, right, barcode_length, out_file)
+parse_tab_file_get_clusters(in_file, left, right, barcode_length, show_me_the_reads, out_file)
 
 print "done"
