20522e425dd93c5d7acb297e70b8ac12dc5c3227
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-10-18 15:48:18 +0100

    trimmo module not working yet

--- santi_script/thapbi_santi/trimmomatic.py
+++ santi_script/thapbi_santi/trimmomatic.py
@@ -45,8 +45,12 @@ class trimmomatic(object):
             self._logger.error("trimmomatic generated some errors")
             sys.exit(1)
         return (self._outdirname, pipe.stdout.readlines())
+    
+#cmd_trimming="java -jar ${trimmomatic_path}/trimmomatic-0.32.jar PE -threads ${num_threads} -phred33 ${left_read_file} ${right_read_file} 
+#${Name_of_project}_R1.fq.gz unpaired_R1.fq.gz ${Name_of_project}_R2.fq.gz unpaired_R2.fq.gz 
+#ILLUMINACLIP:${repository_path}/database_files/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75" 
 
-    def __build_cmd(self, infname, outdir):
+    def __build_cmd(self, L_reads, R_reads, outdir):
         """Build a command-line for trimmomatic"""
         self._outdirname = os.path.join(outdir, "trimmomatic_output")
         cmd = ["trimmomatic",
